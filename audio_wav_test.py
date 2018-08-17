069430dff9f06db9a95c8815a3c35cbd5aedf101
jzerez <Jonathan.zerez@students.olin.edu>
2018-08-17 19:26:51 -0400

    cleaned up wav reading and writing. Started lpass filter

--- audio_wav_test.py
+++ audio_wav_test.py
@@ -2,103 +2,55 @@ import wave
 import struct
 import matplotlib.pyplot as plt
 
-def HexToByte( hexStr ):
+def read_wav(file='Loud_Pipes-BcoPKWzLjrE.wav', num_frames=None, start_pos=0, plot=False, verbose=False):
     """
-    Convert a string hex byte values into a byte string. The Hex Byte values may
-    or may not be space separated.
+    This function takes a wave file and returns an tuple of amplitude integers
+    corresponding to the bytes written in the wave file. Returns both left and
+    right channels in the same array if the wave file is in stereo.
     """
-    # The list comprehension implementation is fractionally slower in this case
-    #
-    #    hexStr = ''.join( hexStr.split(" ") )
-    #    return ''.join( ["%c" % chr( int ( hexStr[i:i+2],16 ) ) \
-    #                                   for i in range(0, len( hexStr ), 2) ] )
+    f = wave.open(file, 'rb')
+    if verbose:
+        print('file params are: ', f.getparams())
 
-    bytes = []
+    if num_frames == None:
+        num_frames = f.getnframes()
+    f.setpos(start_pos)
 
-    hexStr = ''.join( hexStr.split(" ") )
+    raw_frames = f.readframes(num_frames)
 
-    for i in range(0, len(hexStr), 2):
-        bytes.append( chr( int (hexStr[i:i+2], 16 ) ) )
+    if f.getsampwidth() == 1:
+        num_samples = len(raw_frames)
+        assert num_samples % 0 == 0, "Number of frames and sample width don't match"
+        int_frames = struct.unpack(str(int(num_samples)) + 'b', raw_frames)
+    elif f.getsampwidth() == 2:
+        num_samples = len(raw_frames) / 2
+        assert num_samples % 1 == 0, "Number of frames and sample width don't match"
+        integer_frames = struct.unpack(str(int(num_samples)) + 'h', raw_frames)
 
-    return ''.join( bytes )
+    if plot:
+        plt.plot(integer_frames)
+        plt.show()
+    return integer_frames
 
-# 127249
-CHUNK = 44100
-#
-f = wave.open('Loud_Pipes-BcoPKWzLjrE.wav', 'rb')
-f.setpos(69000)
-#
-b = f.readframes(CHUNK)
-print(b[0:50])
-danny = struct.unpack(str(int(len(b)/2)) + 'h', b)
+def write_wav(integer_frames, num_channels=2, sample_width=2, framerate=44100, file='test.wav'):
+    """
+    This function takes a tuple of amplitude intitude integers, converts them
+    into a byte string, and generates a wav file. 
+    """
+    num_frames = int(len(integer_frames) / num_channels)
 
-robbie = struct.unpack(str(int(len(b))) + 'b', b)
-plt.plot(danny[::2])
-plt.show()
+    if sample_width == 1:
+        bytes = struct.pack(str(len(integer_frames)) + 'b', *integer_frames)
+    elif sample_width == 2:
+        bytes = struct.pack(str(len(integer_frames)) + 'h', *integer_frames)
 
-print('DANNY')
-print(danny[0:100])
-print('raw data ', danny[0])
-print('hexxed data ', hex(danny[0]))
-print('byted data ???: ', hex(danny[0]).encode('utf-8'))
-print(max(danny))
-print(len(danny))
+    f = wave.open(file, 'wb')
+    f.setnchannels(num_channels)
+    f.setsampwidth(sample_width)
+    f.setframerate(framerate)
+    f.setnframes(num_frames)
+    f.writeframes(bytes)
 
-print('ROBERT')
-robert = struct.pack('5'+'h', (*danny[0:5]))
-print('robert type: ', type(robert))
-print('robert len: ', len(robert))
-print('one of robert: ', robert)
-
-final = struct.pack(str(len(danny[::2])) + 'h', *danny[::2])
-
-
-# print(b)
-# print(len(b))
-# print(struct.unpack('4b', f.readframes(1)))
-# print(f.readframes(10000).decode('utf-8'))
-# print(type(f.readframes(25)))
-# t = ()
-# for substring in (str(f.getparams()).split('(')[1].split(', ')):
-#     t += (substring.split(')')[0].split('=')[1],)
-# print(t)
-# print(f.getparams())
-# f.setpos(60000)
-# # for i in range(30):
-# #     b = f.readframes(1)
-# #     data = struct.unpack(str(len(b)) + 'b', b)
-# #     print(b)
-# #     print(data)
-#
-# read_data = (f.readframes(44100))
-# print(type(read_data))
-# write_data1 = read_data[::4]
-# write_data2 = read_data[1::4]
-# write_data = [None] * (len(write_data1) + len(write_data2))
-# write_data[::2] = write_data1
-# write_data[1::2] = write_data2
-#
-# print(type(write_data[1]))
-
-
-# help = struct.unpack(str(int(len(final)/4)) + 'i', final)
-# print(help[:20])
-# plt.plot(help)
-# plt.show()
-# for data in read_data[::2]:
-#     if i % 2 == 0:
-#         write_data += hex(data).encode('utf-8')
-#         write_data += hex(read_data[i+1]).encode('utf-8')
-#     i += 1
-
-print(type(final))
-
-testf = wave.open('test.wav', 'wb')
-testf.setnchannels(1)
-testf.setsampwidth(f.getsampwidth())
-testf.setframerate(f.getframerate()/1)
-testf.setnframes(len(danny[::2]))
-
-print(testf.tell())
-testf.writeframes(final*10)
-print('hi')
+if __name__ == "__main__":
+    res = read_wav(num_frames=44100 * 5, start_pos=69000, plot=True)
+    write_wav(res, file='new.wav')
